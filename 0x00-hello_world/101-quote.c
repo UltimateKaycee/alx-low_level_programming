710fe2c50ae73c2f0cfb475f6fb32e5bb0a197a6
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-10 16:16:31 -0700

    A C program to print some texts to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*Main - Entry point
+*Description: 'program prints some texts to the standard error'
+*Return: Always 1 (Success)
+*/
+int main(void)
+/*
+*function to print some texts
+*using to the standard error
+*/
+{printf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+return (1);
