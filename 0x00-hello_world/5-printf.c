d3bca5154ffa1c1d807e8e8e5f8f018499b3ebf0
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-10 13:12:40 -0700

    adjusted for checker

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@ int main(void)
 *function to print some texts
 *using printf
 */
-{printf(stderr, "%s", "with proper grammar, but the outcome is a piece of art,\n ");
+{printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
