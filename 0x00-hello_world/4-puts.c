43440116e6784026e6c0dff8489ec7ad98650830
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-10 10:52:22 -0700

    correcting the c program printing text lastly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void) { 
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return 0;
+int main(void) 
+#function to print out some texts to the output
+{ 
+puts("\"Programming is like building a multilingual puzzle"); 
+return 0;
 }
