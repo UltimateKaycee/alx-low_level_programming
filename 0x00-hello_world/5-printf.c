7cd7475209b4669a3952031abbff715942f05096
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-10 11:53:23 -0700

    program printing some texts with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'program prints some texts with printf'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+/*
+*function to print some texts
+*using printf
+*/
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+~                                                                                                                                                    ~       
