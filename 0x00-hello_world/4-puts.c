44c0b6c975ac30e5e26ce3f9f712aec522e2698e
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-10 11:37:04 -0700

    correcting the c program printing text lastly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-/*
-*function to
-*print texts
-*/
+/**
+ *main - Entry point
+ * 
+ *Description: 'program prints some texts without printf'
+ *@parameter: There is no parameter
+ * 
+ *Return: Always 0 (Success)
+ */
 int main(void)
 /*
 *function to print some texts
