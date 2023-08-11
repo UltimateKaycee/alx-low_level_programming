703c1cdd240e54700a16a45f0f2553737e5f0b12
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-11 07:17:18 -0700

    To print alphabets in lowecase followed by a newline

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*Description: "prints the alphabet in lowercase, and then in uppercase, followed by a new line"
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int cha;
+for (cha = 'a'; cha <= 'z'; cha++)
+putchar(cha);
+for (cha = 'A'; cha <= 'Z'; cha++)
+putchar(cha);
+putchar('\n');
+return (0);
+}
