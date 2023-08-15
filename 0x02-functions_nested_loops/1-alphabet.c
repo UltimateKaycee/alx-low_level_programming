1607028e510f49a626a237a73693e75f885c94d0
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-15 16:00:29 -0700

    wrote a function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*main: Entry point
-*Description: "a function that prints the alphabet, in lowercase, followed by a new line"
+*main - Entry point
+*" Description: a function that prints the alphabet, in lowercase, followed by a new line"
 *Return: Always 0 (success)
 */
 void print_alphabet(void)
