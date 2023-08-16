92fdcca2cc24fce6ad8dd48b856bc425bd1ddcfa
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-16 02:52:26 -0700

    first debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - function checks to see that positive or negative
+* gives correct output
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int vai;
+
+	vai = 0;
+
+	positive_or_negative(vai);
+
+	return (0);
+}
