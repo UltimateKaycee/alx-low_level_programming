681365df5b08a2571b1d681834234b36f5de8aa4
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-15 19:34:37 -0700

    function adds two integers and returns result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,14 @@
 * add - adds two integers and returns the result
 * @ana: int to be added to bana
 * @bana: int to be added to ana
+* 
 * Return: sum of ana and bana
 */
-int add(int ana, int bana)
+int add (int ana, int bana)
 {
 	int cata;
-	
+
 	cata = ana + bana;
-	
+
 	return (cata);
 }
