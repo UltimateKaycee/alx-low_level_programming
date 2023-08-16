cb84d20ce9c76937e28b58dc1c7bdec9048cbca0
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-16 03:50:22 -0700

    lines of code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,12 @@ int main(void)
 	int vai;
 
 	printf("Infinite loop incoming :(\n");
-
 	vai = 0;
 
 	/* while (vai < 10) */
 	/* { */
 	/* putchar(vai); */
 	/* } */
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
