060dd744cbe982eacf6da831c734fb0e3a610009
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-15 17:59:56 -0700

    function to print 10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,24 +7,18 @@
 */
 
 void print_alphabet_x10(void)
+
 {
 	char ch = 'a';
-	
 	int i = 0;
-	
 	for (; i < 10; i++)
 	{
-	
 	while (ch <= 'z')
 	{
-	
 	_putchar(ch);
-	
 	ch++;
 	}
-	
 	ch = 'a';
-	
 	_putchar ('\n');
 	}
 }
