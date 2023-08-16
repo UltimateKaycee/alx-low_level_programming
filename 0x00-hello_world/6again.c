ff10c165d6055184ce73818dd3b1a20cae7ba011
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-15 17:22:31 -0700

    all others

---
+++ 0x00-hello_world/6again.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: "Print some strings with a new line"
+*Return: Always 0 (success)
+*/
+int main (void)
+{
+    printf("The size of type int is %d:", sizeof(int));
+    printf("The size of type char is %c:", sizeof(char));
+    printf("The size of type long int is %d:", sizeof(long int));
+    printf("The size of type long long int is %d:", sizeof(long long int));
+    printf("The size of type float is %f:", sizeof(int));
+}
+
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+printf("1234 This is how we do!");	
+return (0);
+}
