1aaa549c06ceef6d59d7754c5a41007292249909
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-15 17:44:25 -0700

    correcting description warning from betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 * main - Entry point
 * Description: A function that prints the alphabet,
-* in lowercase, followed by a new line
+* in lowercase, followed by a new line.
 * Return: Always 0 (success)
 */
 
