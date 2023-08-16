b858045db3b42cd0c4409ddeb27c8af96a59df01
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-16 03:15:18 -0700

    tackled the 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - function that causes an infinite loop
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int vai;
+
+	printf("Infinite loop incoming :(\n");
+
+	vai = 0;
+
+	/* while (vai < 10)*/
+	/* {*/
+	/*	putchar(vai);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
