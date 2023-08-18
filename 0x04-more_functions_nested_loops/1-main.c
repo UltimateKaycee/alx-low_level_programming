994328f1d26bc7e87f7709373dc0dc5ee9cc0e09
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 16:10:53 -0700

    Tackled 1-isdigit tasks

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	    char rooc;
+
+	        rooc = '0';
+		    printf("%c: %d\n", rooc, _isdigit(rooc));
+		       rooc = 'a';
+			    printf("%c: %d\n", rooc, _isdigit(rooc));
+			        return (0);
+}
