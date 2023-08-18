b5175f41f10441000d4c39ecb5d93d173fe6dee6
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 16:57:43 -0700

    Tackled the fourth tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function prints the numbers,
- * from 0 to 9, followed by a new line
+ * print_most_numbers - function prints the
+ * numbers from 0 to 9, followed by a new line
  * @bright: parameter
+ *
  * Return: nothing
  */
 void print_most_numbers(void)
