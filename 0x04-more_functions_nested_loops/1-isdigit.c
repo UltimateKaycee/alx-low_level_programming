d8782c0c40579972942d404097c9af00a315c8e1
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 16:06:09 -0700

    Tackled 1-isdigit tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - A function to check for digit (0 through 9).
+ * @rooc: number of int type.
+ * Return: 1 if rooc is a digit, 0 otherwise
+ */
+int _isdigit(int rooc)
+{
+	if (rooc >= 48 && rooc <= 57)
+
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
