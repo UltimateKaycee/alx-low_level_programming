a0aae469954cd15bfc34617ba3062de3204a40eb
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 16:38:21 -0700

    3rd one

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two integers.
+ * @daa: number of type int
+ * @baa: number of tyep int
+ * Return: daaa * baa
+ */
+int mul(int daa, int baa)
+{
+	return (daa * baa);
+}
