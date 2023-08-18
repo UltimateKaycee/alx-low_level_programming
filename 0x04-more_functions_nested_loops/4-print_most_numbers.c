bc0874f8e90e3f7a1e2a003f528d3498fff1cc29
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 16:55:32 -0700

    Tackled the fourth tasks

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function prints the numbers,
+ * from 0 to 9, followed by a new line
+ * @bright: parameter
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int bright;
+
+	for (bright = 48; bright < 58; bright++)
+	{
+	if (bright != 50 && bright != 52)
+	{
+	_putchar(bright);
+	}
+	}
+	_putchar(10);
+}
