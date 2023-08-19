7fc40936cab2633929678191f82b6c21096f3f57
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 18:07:13 -0700

    tackled task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - function to create fizz buzz from numbers 1 to 100
+ * Description: function to print fizz for multiples of 3,
+ * Buzz for multiples of 5, and
+ * 3 and 5 for multiples of both 3 and 5.
+ * Return: 0
+ */
+int main(void)
+{
+	int bright;
+	bright = 1;
+	while (bright <= 100)
+	{
+	if (bright % 3 == 0 && bright % 5 == 0)
+	printf("FizzBuzz");
+	else if (bright % 3 == 0)
+	printf("Fizz");
+	else if (bright % 5 == 0)
+	printf("Buzz");
+	else
+	printf("%d", bright);
+	if (bright != 100)
+	printf(" ");
+	bright++;
+	}
+	printf("\n");
+	return (0);
+}
