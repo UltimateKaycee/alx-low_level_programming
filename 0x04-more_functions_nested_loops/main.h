6a5a767e73e4729448aeec9af54a131070fa6f74
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-18 18:59:04 -0700

    revisited task 1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-int _putchar(char rooc);
-int _isupper(int rooc);
-int _isdigit(int rooc);
-int mul(int daa, int baa);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int nos);
-void print_diagonal(int nos);
+void print_line(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int nos);
+void print_number(int n);
 #endif
