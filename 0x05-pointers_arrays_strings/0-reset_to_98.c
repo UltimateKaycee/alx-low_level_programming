4989f200c0aa957942a49b69dbe6cf1d70bb7dc5
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-21 08:54:05 -0700

    Fixed the task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function taking  a pointer 
+ * reset_to_98 - a function taking a pointer
  * to an int as a parameter and updating
  * the value it points to to 98
  * @number: int pointer variable
  *
  * Return - nothing
  */
-
 void reset_to_98(int *number)
 {
-		*number = 98;
+	*number = 98;
 }
