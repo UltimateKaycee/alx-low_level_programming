f4edc0a4aaac8b19848d63a3709821bc53b193fe
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-23 15:06:01 -0700

    Adjusted a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - function to concatenate two strings
  * @dest: string
  * @src: string
- * @blink number of items to concatenate
+ * @blink: number of items to concatenate
  * Return: `dest`
  */
 
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int blink)
 	int high, dash;
 
 	for (high = 0; dest[high] != '\0'; high++)
-	;
+		;
 	for (dash = 0; src[dash] != '\0' && blink > 0; dash++, blink--, high++)
 	{
 	dest[high] = src[dash];
