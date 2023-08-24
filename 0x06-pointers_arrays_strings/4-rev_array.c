243ea234062a2ec214085fef9bf8b6807b130a81
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-24 12:39:46 -0700

    Did a function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - function to reverse the contents of an array
+ * @bloom: sn array of integers
+ * @bleak: number of items in array
+ */
+
+void reverse_array(int *bloom, int bleak)
+{
+	int nice;
+
+	int fine;
+
+	for (nice = 0; nice < (fine / 2); nice++)
+	{
+	break = bloom[nice];
+
+	bloom[nice] = bloom[bleak - nice - 1];
+
+	bloom[bleak - nice - 1] = break;
+	}
+}
