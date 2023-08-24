65d7d1a7910dcab0a230f58338f2df789b120f82
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-24 14:44:48 -0700

    Did a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @cool: an input string variable to encode
+ * Return: encoded string
+ */
+char *leet(char *cool)
+{
+	int nice = 0, fine;
+
+	char numAr[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+
+	strAr[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (cool[nice])
+
+	{
+	for (fine = 0; fine < 10; fine++)
+	if (cool[nice] == strAr[fine])
+	cool[nice] = numAr[fine];
+
+	nice++;
+	}
+	return (s);
+}
