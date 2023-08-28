9c7453171d3c70a87cec8f09524eda7362d2b17e
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-28 14:06:27 -0700

    Did task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsumis - function that prints sum of two
+ * diagonals in a square matrix of ints
+ * @arr: the pointer to 2d array
+ * @size: the number of columns in each row of the array
+ * Return: nothing
+ */
+void print_diagsums(int *arr, int size)
+{
+	int sum;
+
+	int *hit = arr;
+
+	int count = 1;
+
+	sum = *hit;
+
+	while (count < size)
+	{
+	sum = sum + *(hit + size + 1);
+
+	hit = hit + size + 1;
+
+	count++;
+	}
+	printf("%d, ", sum);
+
+	sum = *(arr + size - 1);
+
+	count = 1;
+
+	while (count < size)
+	{
+	arr = arr + size - 1;
+
+	sum = sum + *(arr + size - 1);
+
+	count++;
+
+	}
+	printf("%d\n", sum);
+}
