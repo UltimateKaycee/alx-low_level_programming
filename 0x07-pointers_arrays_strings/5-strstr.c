0a6acd8a254204fee7cfc3c157769902a1b793d8
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-28 09:50:39 -0700

    adjusted task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,6 +7,7 @@
  * @num: pointer to second string
  * Return: integer 1 for a match or 0 for no match
  */
+
 int match_found(char *gloom, char *num)
 {
 	if (*gloom == *num)
@@ -22,11 +23,13 @@ int match_found(char *gloom, char *num)
  * @needle: substring to be found
  * Return: pointer to beginning of located substring or NULL if not found
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int num, gloom, catch, match;
 
 	gloom = 0;
+
 	while (haystack[gloom] != '\0')
 	{
 	num = 0;
@@ -38,6 +41,7 @@ char *_strstr(char *haystack, char *needle)
 	while (match == 1 && needle[num] != '\0')
 	{
 	match = match_found((haystack + catch), (needle + num));
+
 	if (needle[num + 1] == '\0' && match == 1)
 	{
 	if (*needle == haystack[gloom])
