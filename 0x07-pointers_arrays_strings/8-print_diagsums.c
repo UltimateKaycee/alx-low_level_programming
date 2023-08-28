5f051e50421d7cb3b3e254e16b30c152ccebf988
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-28 12:04:22 -0700

    Did a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function to print an
+ * array of characters as a chessboard
+ * @arr: array to be printed
+ * Return: nothing
+ */
+void print_chessboard(char (*arr)[8])
+{
+	int row = 0;
+
+	int col;
+
+	while (row < 8)
+	{
+	col = 0;
+
+	while (col < 8)
+	{
+	if (col == 7)
+	{
+	_putchar(arr[row][col]);
+	_putchar('\n');
+	}
+	else
+	_putchar(arr[row][col]);
+
+	col++;
+	}
+	row++;
+}
