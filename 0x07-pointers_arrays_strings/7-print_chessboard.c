25f30634ebf6f18b52f9e4e72a04bb78a708b10c
ultimatekaycee <ultimatekaycee@outlook.com>
2023-08-29 14:18:08 -0700

    adjusted task chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,10 +3,10 @@
 /**
  * print_chessboard - function to print an
  * array of characters as a chessboard
- * @arr: array to be printed
+ * @arr1: array to be printed
  * Return: nothing
  */
-void print_chessboard(char (*arr)[8])
+void print_chessboard(char (*arr1)[8])
 {
 	int row = 0;
 
@@ -20,16 +20,16 @@ void print_chessboard(char (*arr)[8])
 	{
 	if (col == 7)
 	{
-	_putchar(arr[row][col]);
+	_putchar(arr1[row][col]);
+
 	_putchar('\n');
 	}
 	else
 
-	_putchar(arr[row][col]);
+	_putchar(arr1[row][col]);
 
 	col++;
-
+	
 	}
-
 	row++;
 }
