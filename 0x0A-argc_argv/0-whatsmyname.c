0e4a0373b369ab62a1a29e516ecd1fa2350ee045
ultimatekaycee <ultimatekaycee@outlook.com>
2023-09-03 09:22:53 -0700

    Did a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function prints program name, followed by new line.
+ * @argc: number of arguments in the program.
+ * @argv: array of pointers to arguments.
+ *
+ * Return: Always o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a directory in the repo about argc and argv
