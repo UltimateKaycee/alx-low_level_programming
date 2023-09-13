fd0f45d8dd583b43c565d6ec847d607ed56267d5
ultimatekaycee <ultimatekaycee@outlook.com>
2023-09-13 08:24:51 -0700

    Did a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - function that prints a name
- * @name: This is a name to printf
+ * @name: This is a name to print
  * @f: pointer to function that prints a name
- *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
