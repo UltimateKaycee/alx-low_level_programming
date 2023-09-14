561c3bb901af93a81b33df62246a927ef050fb8b
ultimatekaycee <ultimatekaycee@outlook.com>
2023-09-14 07:25:52 -0700

    Did a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,26 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+#include <stdarg.h>
 
-typedef struct validTypes
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
+typedef struct printer
 {
-	char *valid;
+	char *symbol;
 
-	void (*f)();
-} v_types;
+	void (*print)(va_list arg);
 
-int _putchar(char c);
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
 
-#endif;
+#endif
