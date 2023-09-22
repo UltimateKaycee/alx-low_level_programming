ee741bed90c621f915a133cac2e796bc32c634ec
ultimatekaycee <ultimatekaycee@outlook.com>
2023-09-22 07:22:08 -0700

    Did a function that frees a list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,12 +4,13 @@
 /**
  * free_list - function to free a
  * linked list
- * @head: the beginning of the list
+ * @head: the start of the list
  */
 void free_list(list_t *head)
 {
 	if (head == NULL)
 	return;
+
 	free_list(head->next);
 
 	free(head->str);
