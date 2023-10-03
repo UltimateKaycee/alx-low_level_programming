0939f8f935b7670dc978561e7aa8b7d29251f5b0
ultimatekaycee <ultimatekaycee@outlook.com>
2023-10-02 18:04:31 -0700

    Did a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,6 +25,7 @@ int main(int ac, char *av[])
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	to_file = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+
 	if (to_file == -1)
 
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
@@ -34,18 +35,21 @@ int main(int ac, char *av[])
 	while (status_one)
 	{
 	status_one = read(from_file, buffer, BUFSIZE);
+
 	if (status_one == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	if (status_one > 0)
 	{
 	status_one = write(to_file, buffer, status_one);
+
 	if (status_two != status_one || status_two == -1)
+
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	}
 	if (close(from_file) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file), exit(100);
 	if (close(to_file) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_file), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_file), exit(100);
 	return (0);
 }
