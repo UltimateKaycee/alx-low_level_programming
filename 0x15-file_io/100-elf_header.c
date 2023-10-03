c6de055a0b97026e495088733a7b4f6469891e1b
ultimatekaycee <ultimatekaycee@outlook.com>
2023-10-02 18:26:37 -0700

    Did a program that displays the information contained in the ELF header at the start of an ELF file.

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * placeholder2 - function to display information
+ * contained in the ELF header at
+ * the start of an EF file
+ * @void: argument
+ * Return: 0 on success
+ */
+int placeholder2(void)
+{
+	return (0);
+}
