be61bdc719311161f590a11480406d4786f3ac19
ultimatekaycee <ultimatekaycee@outlook.com>
2023-10-02 18:21:57 -0700

    Did a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,9 @@ int main(int ac, char *av[])
 	status_one = read(from_file, buffer, BUFSIZE);
 
 	if (status_one == -1)
+
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	if (status_one > 0)
 	{
 	status_two = write(to_file, buffer, status_one);
@@ -45,11 +47,15 @@ int main(int ac, char *av[])
 	if (status_two != status_one || status_two == -1)
 
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
 	}
 	}
 	if (close(from_file) == -1)
+
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file), exit(100);
+
 	if (close(to_file) == -1)
+
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_file), exit(100);
 	return (0);
 }
