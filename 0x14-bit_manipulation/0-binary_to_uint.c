c179a4ecca8f859669c4b52738c7f929fb39276c
ultimatekaycee <ultimatekaycee@outlook.com>
2023-11-23 18:09:42 -0800

    Did a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,47 @@
-include "main.h"
+#include "main.h"
 /**
- * binary_to_uint - function to converts
- * binary number to an unsigned int.
- * @b: pointer to string of zero (0) and
- * one (1) chars.
- *
- * Return: 0 if b is NULL or contains
- * chars not 0 or 1
- * Otherwise - the number.
- */
+  * _stoi - to conv char to int
+  * @c: char to convert
+  * Return: the int
+  */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+  * _strlen - to calc len of string
+  * @s: input
+  * Return: len of string
+  */
+unsigned int _strlen(const char *s)
+{
+	unsigned int high;
+
+	for (high = 0; s[high]; high++)
+		;
+	return (high);
+}
+/**
+  * binary_to_uint - to conv string of ones and
+  * zeros to decimal number
+  * @b: string to conv
+  * Return: unsigned decimal num
+  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number = 0, fab = 1;
-	int length;
+	int high;
+	unsigned int output, temp, rival;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-
-	for (length = 0; b[length];)
-		length++;
-
-	for (length -= 1; length >= 0; length--)
+	output = temp = 0;
+	rival = 1;
+	for (high = _strlen(b) - 1; b[high]; high--, rival *= 2)
 	{
-		if (b[length] != '0' && b[length] != '1')
+		if (b[high] != '0' && b[high] != '1')
 			return (0);
-
-		number += (b[length] - '0') * fab;
-		fab *= 2;
+		temp = _stoi(b[high]);
+		output += temp * rival;
 	}
-
-	return (number);
+	return (output);
 }
