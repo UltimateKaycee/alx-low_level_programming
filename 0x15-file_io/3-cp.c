af36ca5f3c8633b2e7a2ca2f886bc312a532e888
ultimatekaycee <ultimatekaycee@outlook.com>
2023-11-27 14:16:39 -0800

    Did a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 /**
- * create_buffer - Function to allocate 1024 bytes
- * for a buffer.
+ * create_buffer - Function to allocate
+ * 1024 bytes for a buffer.
  * @file: Name of file buffer stores chars for.
  *
  * Return: ptr to newly-allocated buffer.
@@ -17,16 +17,17 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
+	dprintf(STDERR_FILENO,
+	"Error: Can't write to %s\n", file);
+	exit(99);
 	}
 
 	return (buffer);
 }
 
 /**
- * close_file - function to close file descriptors.
+ * close_file - function to close file
+ * descriptors.
  * @fd: file descriptor
  */
 void close_file(int fd)
@@ -37,8 +38,8 @@ void close_file(int fd)
 
 	if (diver == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 	}
 }
 
@@ -58,8 +59,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp source_file dest_file\n");
-		exit(97);
+	dprintf(STDERR_FILENO, "Usage: cp source_file dest_file\n");
+	exit(97);
 	}
 
 	buffer = create_buffer(argv[2]);
@@ -71,27 +72,27 @@ int main(int argc, char *argv[])
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (source == -1 || reader == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
+	if (source == -1 || reader == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't read from file %s\n", argv[1]);
+	free(buffer);
+	exit(98);
+	}
 
-		writer = write(dest, buffer, reader);
+	writer = write(dest, buffer, reader);
 
-		if (dest == -1 || writer == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
+	if (dest == -1 || writer == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't write to %s\n", argv[2]);
+	free(buffer);
+	exit(99);
+	}
 
-		reader = read(source, buffer, 1024);
+	reader = read(source, buffer, 1024);
 
-		dest = open(argv[2], O_WRONLY | O_APPEND);
+	dest = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (reader > 0);
 
